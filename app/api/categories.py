bff223e1707e56d30f2baaf4dcca66b379c6271d
Angela Cafà <angela97.cafa@gmail.com>
2025-11-24 11:02:01 +0100

    del tag

--- app/api/categories.py
+++ app/api/categories.py
@@ -4,7 +4,9 @@ from app.db_connection import get_db
 from app.schemas.category import CategoryResponse
 from app.crud import category as crud_category
 
-router = APIRouter(prefix="/categories", tags=["categories"])
+router = APIRouter(prefix="/categories", 
+                  # tags=["categories"]
+                   )
 
 @router.get("/", response_model=list[CategoryResponse])
 async def get_categories(db: Session = Depends(get_db)):
